3d336b4894a7058dd98092af95c764241670c975
Dasari Venkata Siddartha Kumar <dasarivenkata.kumar@perficient.com>
2023-08-28 16:23:13 +0530

    user profile bug fixed

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -25,24 +25,52 @@ const ProfileScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [address, setAddress] = useState('');
+  const [address1, setAddress1] = useState('');
+  const [address2, setAddress2] = useState('');
   const [phonenumber, setPhonenumber] = useState('');
+  const [activeTab, setActiveTab] = useState('profile');
 
   const { userInfo } = useSelector((state) => state.auth);
 
   const { data: orders, isLoading, error } = useGetMyOrdersQuery();
 
-  const [updateProfile, { isLoading: loadingUpdateProfile }] =
-    useProfileMutation();
+  const [updateProfile, { isLoading: loadingUpdateProfile }] = useProfileMutation();
 
   useEffect(() => {
     setName(userInfo.name);
     setEmail(userInfo.email);
-    setAddress(userInfo.address);
+    setAddress1(userInfo.address1);
+    setAddress2(userInfo.address2);
     setPhonenumber(userInfo.phonenumber);
-  }, [userInfo.email, userInfo.name, userInfo.address, userInfo.phonenumber]);
+  }, [userInfo.email, userInfo.name, userInfo.address1, userInfo.address2, userInfo.phonenumber]);
 
   const dispatch = useDispatch();
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  const handleEditAddress = (field) => {
+   
+    if (field === 'address1') {
+      setAddress1EditMode(true);
+    } else if (field === 'address2') {
+      setAddress2EditMode(true);
+    }
+  };
+
+  const handleSaveAddress = async (field) => {
+    
+    if (field === 'address1') {
+      setAddress1EditMode(false);
+    } else if (field === 'address2') {
+      setAddress2EditMode(false);
+    }
+  };
+
+  const [isEditingAddress1, setAddress1EditMode] = useState(false);
+  const [isEditingAddress2, setAddress2EditMode] = useState(false);
+
   const submitHandler = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
@@ -54,7 +82,8 @@ const ProfileScreen = () => {
           name,
           email,
           password,
-          address,
+          address1,
+          address2,
           phonenumber,
         }).unwrap();
         dispatch(setCredentials({ ...res }));
@@ -81,72 +110,138 @@ const ProfileScreen = () => {
               <b>Profile</b>
             </h3>
             <Form onSubmit={submitHandler}>
-              <Form onSubmit={submitHandler}>
-                <Form.Group className='my-2' controlId='name'>
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control
-                    type='name'
-                    placeholder='Enter name'
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
+             
+              <Form.Group className='my-2' controlId='name'>
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Enter name'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Form.Group>
 
-                <Form.Group className='my-2' controlId='email'>
-                  <Form.Label>Email Address</Form.Label>
-                  <Form.Control
-                    type='email'
-                    placeholder='Enter email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
+              <Form.Group className='my-2' controlId='email'>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  type='email'
+                  placeholder='Enter email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className='my-2' controlId='password'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type='password'
+                  placeholder='Enter password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
 
-                <Form.Group className='my-2' controlId='password'>
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type='password'
-                    placeholder='Enter password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
+              <Form.Group className='my-2' controlId='confirmPassword'>
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type='password'
+                  placeholder='Confirm password'
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </Form.Group>
 
-                <Form.Group className='my-2' controlId='confirmPassword'>
-                  <Form.Label>Confirm Password</Form.Label>
-                  <Form.Control
-                    type='password'
-                    placeholder='Confirm password'
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
+              
+              <Form.Group className='my-2' controlId='address1'>
+                <Form.Label>Home Address</Form.Label>
+                {isEditingAddress1 ? (
+                  <>
+                    <Form.Control
+                      type='text'
+                      as="textarea"
+                      placeholder='Enter Home Address'
+                      value={address1}
+                      onChange={(e) => setAddress1(e.target.value)}
+                    />
+                    <Button
+                      variant='success'
+                      className='mt-1'
+                      onClick={() => handleSaveAddress('address1')}
+                    >
+                      Save
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <Form.Control
+                      type='text'
+                      value={address1}
+                      readOnly
+                    />
+                    <Button
+                      variant='primary'
+                      onClick={() => handleEditAddress('address1')}
+                      className='mt-1'
+                    >
+                      Edit
+                    </Button>
+                  </>
+                )}
+              </Form.Group>
 
-                <Form.Group className='my-2' controlId='Address'>
-                  <Form.Label>Address</Form.Label>
-                  <Form.Control
-                    type='address'
-                    as='textarea'
-                    placeholder='Address'
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
-                <Form.Group className='my-2' controlId='Phonenumber'>
-                  <Form.Label>Phone Number</Form.Label>
-                  <Form.Control
-                    type='text'
-                    placeholder='Phone Number'
-                    value={phonenumber}
-                    onChange={(e) => setPhonenumber(e.target.value)}
-                  ></Form.Control>
-                </Form.Group>
+              
+              <Form.Group className='my-2' controlId='address2'>
+                <Form.Label>Office Address</Form.Label>
+                {isEditingAddress2 ? (
+                  <>
+                    <Form.Control
+                      type='text'
+                      as="textarea"
+                      placeholder='Enter Office Address'
+                      value={address2}
+                      onChange={(e) => setAddress2(e.target.value)}
+                    />
+                    <Button
+                      variant='success'
+                      onClick={() => handleSaveAddress('address2')}
+                      className='mt-1'
 
-                <Button type='submit' variant='primary'>
-                  Update
-                </Button>
-                {loadingUpdateProfile && <Loader />}
-              </Form>
+                    >
+                      Save
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <Form.Control
+                      type='text'
+                      value={address2}
+                      readOnly
+                    />
+                    <Button
+                      variant='primary'
+                      className='mt-1'
+                      onClick={() => handleEditAddress('address2')}
+                    >
+                      Edit
+                    </Button>
+                  </>
+                )}
+              </Form.Group>
+
+              <Form.Group className='my-2' controlId='Phonenumber'>
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Phone Number'
+                  value={phonenumber}
+                  onChange={(e) => setPhonenumber(e.target.value)}
+                />
+              </Form.Group>                                                 
+
+              <Button type='submit' variant='primary'>
+                Update
+              </Button>
+              {loadingUpdateProfile && <Loader />}
+              
             </Form>
           </Tab>
           <Tab eventKey='orders' title='Orders' disabled={userInfo.isAdmin}>
